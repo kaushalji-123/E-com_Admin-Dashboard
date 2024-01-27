21e51bb710c7ff6b30400cfbf3c4e06c8c833a5b
kaushalji-123 <divyanshu838283fb@gmail.com>
2024-01-27 23:49:23 +0530

    dded bar

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,12 +1,19 @@
 import AdminSidebar from "../Components/AdminSidebar"
- 
+import { BsSearch } from "react-icons/bs" 
+import { FaRegBell } from "react-icons/fa"
+import user from "../assets/user.png";
+
 const Dashboard = () => {
   return (
     <div className="admin-container">
     <AdminSidebar/>
-      <main className="dashhboard">
+      <main className="dashboard">
         <div className="bar">
-          bar
+
+          <BsSearch/>
+          <input type="text" placeholder="Search for data , User" />
+          <FaRegBell/>
+          <img src={user} alt="User" />
         </div>
       </main>
     </div>
