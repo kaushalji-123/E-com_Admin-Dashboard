4f13e16d650add13d0472b568fe0ebaadd72625e
kaushalji-123 <divyanshu838283fb@gmail.com>
2024-01-30 18:44:46 +0530

    ATM

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,7 +1,11 @@
 import AdminSidebar from "../Components/AdminSidebar"
+// React icons
 import { BsSearch } from "react-icons/bs" 
 import { FaRegBell } from "react-icons/fa"
-import user from "../assets/user.png";
+import { HiTrendingUp,HiTrendingDown } from "react-icons/hi";
+
+import userImg from "../assets/user.png";
+
 
 const Dashboard = () => {
   return (
@@ -13,11 +17,31 @@ const Dashboard = () => {
           <BsSearch/>
           <input type="text" placeholder="Search for data , User" />
           <FaRegBell/>
-          <img src={user} alt="User" />
+          <img src={userImg} alt="User" />
         </div>
 
         <section className="widgetcontainer">
-
+          <WidgetItem 
+          percent={40}
+          amount={true}
+          value={40}
+          heading="340000"
+          color="rgb(0,115,255)"
+          />
+          <WidgetItem 
+          percent={-40}
+          amount={true}
+          value={90}
+          heading="340000"
+          color="rgb(0,115,255)"
+          />
+          <WidgetItem 
+          percent={40}
+          amount={true}
+          value={40}
+          heading="340000"
+          color="rgb(0,115,255)"
+          />
         </section>
       </main>
     </div>
@@ -31,8 +55,30 @@ interface WidgetItemProps{
   color:string;
   amount?:boolean;
 }
-const WidgetItem =({heading,value,percent,color,amount}: WidgetItemProps) => (
-  <article className="widget"> </article>
+const WidgetItem =  ({ 
+  heading,
+  value,
+  percent,
+  color,
+  amount 
+}: WidgetItemProps) => (
+  <article className="widget">
+    <div className="widgetInfo">
+      <p>{heading}</p>
+      <h4>Revenue {amount ? `$${value}` : value}</h4> 
+      {
+      percent > 0 ? (
+        <span className="green">
+          <HiTrendingUp /> + {percent}%{""}
+        </span>
+      ) : (
+        <span className="red">
+          <HiTrendingDown /> {percent}%{""}
+        </span>
+      )
+    }
+    </div>
+  </article>
 );
 
 
